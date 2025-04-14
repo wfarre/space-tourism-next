7efd877f523fcd8f7227be846b9e52ad11156642
wfarre <william.farre@gmail.com>
2025-04-14 12:11:54 +0800

    fix: display issue

--- src/app/components/layout/PageBackground.tsx
+++ src/app/components/layout/PageBackground.tsx
@@ -7,29 +7,29 @@ const PageBackground = () => {
   const path = usePathname();
   const pathname = path === "/" ? "home" : path.split("/")[1];
   return (
-    <>
+    <div className="-z-10">
       <Image
-        className="absolute top-0 left-0 -z-10 hidden h-full w-full xl:block"
+        className="absolute top-0 left-0 hidden h-full w-full xl:block"
         src={`/assets/${pathname}/background-${pathname}-desktop.jpg`}
         alt=""
         height={900}
         width={1440}
       />
       <Image
-        className="absolute top-0 left-0 -z-10 hidden h-full w-full md:block xl:hidden"
+        className="absolute top-0 left-0 hidden h-full w-full md:block xl:hidden"
         src={`/assets/${pathname}/background-${pathname}-tablet.jpg`}
         alt=""
         height={1024}
         width={768}
       />
       <Image
-        className="absolute top-0 left-0 -z-10 h-full w-full md:hidden"
+        className="absolute top-0 left-0 h-full w-full md:hidden"
         src={`/assets/${pathname}/background-${pathname}-mobile.jpg`}
         alt=""
         height={667}
         width={375}
       />
-    </>
+    </div>
   );
 };
 
