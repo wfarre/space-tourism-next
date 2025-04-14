c147adbdb3c557aad8c1973df1de21df3ef734ec
wfarre <william.farre@gmail.com>
2025-04-14 16:37:00 +0800

    fix: homepage issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-      <section className="bottom-32 left-0 mx-6 mt-6 flex flex-col items-center justify-between gap-y-30 pb-32 text-center md:mx-[128px] md:mt-[128px] md:gap-y-16 xl:absolute xl:mx-[165px] xl:flex-row xl:text-left">
+      <section className="flex flex-col items-center justify-between gap-y-30 pt-6 pb-32 text-center md:gap-y-16 md:pt-32 xl:flex-row xl:pt-72 xl:text-left">
         <div className="max-w-[512px] xl:w-1/2 xl:max-w-none">
           <h1>
             <span className="font-main block text-base tracking-widest text-slate-300 md:text-[28px]">
@@ -28,7 +28,7 @@ export default function Home() {
         </div>
         <Link
           href={"/destination/moon"}
-          className="font-title relative z-10 flex h-36 w-36 items-center justify-center rounded-full bg-white text-lg tracking-widest text-slate-900 uppercase transition-all duration-300 before:absolute before:top-1/2 before:left-1/2 before:-z-[5] before:h-80 before:w-80 before:-translate-1/2 before:scale-0 before:rounded-full before:bg-white before:opacity-0 before:duration-300 hover:before:scale-100 hover:before:opacity-20 md:h-59 md:w-59 md:text-[32px]"
+          className="font-title relative z-10 flex h-36 w-36 items-center justify-center rounded-full bg-white text-lg tracking-widest text-slate-900 uppercase transition-all duration-300 before:absolute before:top-1/2 before:left-1/2 before:-z-[5] before:h-95 before:w-95 before:-translate-1/2 before:scale-0 before:rounded-full before:bg-white before:opacity-0 before:duration-300 hover:before:scale-100 hover:before:opacity-20 md:h-68 md:w-68 md:text-[32px]"
         >
           Explore
         </Link>
